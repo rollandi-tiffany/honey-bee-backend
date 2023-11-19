181bf0fe0f657f2438bca0117beb0cf6193dbb40
Tiffany Torres <torresrtiffany@gmail.com>
2023-11-19 13:22:34 -0800

    add database

--- honeybackend/settings.py
+++ honeybackend/settings.py
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'honeybackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 

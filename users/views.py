ab11cd39ea97881c9208e1a2af8e76c7c08b0395
Tiffany Torres <torresrtiffany@gmail.com>
2023-11-20 01:03:07 -0800

    update userlogin in views

--- users/views.py
+++ users/views.py
@@ -4,6 +4,10 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .models import User
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -16,9 +20,25 @@ class UserSignup(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserLogin(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserLogin(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Validate if both username and password are provided
+        if not username or not password:
+            return Response({'message': 'Both username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # Login the user
+            login(request, user)
+            return Response({'message': 'Logged in successfully.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid credentials. Please try again.'}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class UserLogout(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()

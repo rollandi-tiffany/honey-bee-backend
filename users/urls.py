90e93bf5b8f1d61ee974f0f5cfb2aad2c14dfc3f
Tiffany Torres <torresrtiffany@gmail.com>
2023-11-20 00:03:59 -0800

    add sitters/ to 2nd path in users url

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from .views import UserLogin, UserSignup, UserLogout, UserView
 
 urlpatterns = [
   path('', include('djoser.urls')),
-  path('', include('djoser.urls.authtoken')),
+  path('sitters/', include('djoser.urls.authtoken')),
   path("signup/", UserSignup.as_view(), name="signup"),
   path("login/", UserLogin.as_view(), name="login"),
   path("logout/", UserLogout.as_view(), name="logout"),
